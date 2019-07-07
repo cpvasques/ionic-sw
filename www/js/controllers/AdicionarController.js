db61f517312ca77320b07ae9d06d8e6b6d8e2733
Cauê Pievetti Vasques <cauev@spi.local>
2019-07-07 15:12:45 -0300

    app de exemplo finalizado

--- www/js/controllers/AdicionarController.js
+++ www/js/controllers/AdicionarController.js
@@ -1,5 +1,17 @@
 angular.module('projetoIonic')
-.controller('AdicionarController', function($scope, $http){
+    .controller('AdicionarController', function ($scope, $http) {
 
+        $scope.adicionar = function () {
 
-});
+            var data = [{
+                "nome": $scope.nome,
+                "classe": $scope.classe,
+                "faccao": $scope.faccao,
+                "descricao": $scope.descricao
+            }];
+
+            $http.post('suaURL', data).then(function (response) {
+                console.log(response);
+            });
+        }
+    });
